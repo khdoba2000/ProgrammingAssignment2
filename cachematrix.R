96cd081200cea0b6c32f8951d458bc87c35246c3
khdoba <kholboevdostonbek@gmail.com>
2020-11-07 00:04:57 +0500

    inserted function codes

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## THIS FUNCTION CREATES A SPESIFIC MATRIX that is list of functions
+## to set and get the actual values, and also inverse of this matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(data) inv <<- data
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
 ## Write a short comment describing this function
+## THIS FUNCTION GETS THE INVERSE OF THE MATRIX FROM CACHE, IF IT IS CALCULATED BEFORE,
+## IF NOT, IT CALCULATES AND SETS IT TO THE MATRIX's ATTRIBUTES
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat)
+  x$setinv(inv)
+  inv
+  
 }
